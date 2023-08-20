7ed3c3b5f33931e5de2a2d17b22ce03ebbd37f51
alpersarac <alper.sarac42@gmail.com>
2023-08-20 21:50:08 +0300

    Walk Model

---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,18 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public double lengthInKm { get; set; }
+        public string? walkImageUrl { get; set; }
+        public Guid difficultyId { get; set; }
+        public Guid regionId { get; set; }
+
+        //Navigation properties
+        public Difficulty difficulty { get; set; }
+        public Region region { get; set; }
+
+    }
+}
