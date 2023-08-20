6b4b8e2623671abad871e27189d2ed6ebece21b8
alpersarac <alper.sarac42@gmail.com>
2023-08-21 01:12:58 +0300

    Getting all and by id methods

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext _dbContext;
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = _dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute]Guid id, string test)
+        {
+           var region = _dbContext.Regions.Find(id);
+            if (region==null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
+        }
+    }
+}
