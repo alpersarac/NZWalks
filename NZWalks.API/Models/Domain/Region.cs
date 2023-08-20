98fb6c3812375bae6b72c469d9760f78a5718f1f
alpersarac <alper.sarac42@gmail.com>
2023-08-20 21:47:36 +0300

    Region Model

---
+++ NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,10 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+        public string? regionImageUrl { get; set; }
+    }
+}
