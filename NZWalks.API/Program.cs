1eba73ed5f63fe977b1c65dfe3a01c2488d5b0f1
alpersarac <alper.sarac42@gmail.com>
2023-08-25 01:54:32 +0300

    DI

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<NZWalksDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 
