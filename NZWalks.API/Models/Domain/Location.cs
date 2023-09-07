5975f59707d3ea8795bc99d807871fd5628681d6
alpersarac <alper.sarac42@gmail.com>
2023-09-08 01:22:58 +0300

    location model

---
+++ NZWalks.API/Models/Domain/Location.cs
@@ -0,0 +1,12 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Location
+    {
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public string coordinates { get; set; }
+
+        public Guid difficultyId { get; set; }
+        public Difficulty difficulty { get; set; }
+    }
+}
