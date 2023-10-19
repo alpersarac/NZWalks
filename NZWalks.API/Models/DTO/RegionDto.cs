2eaac3e43de3df2ad1de935bd4e036a12f09145c
alpersarac <alper.sarac42@gmail.com>
2023-10-19 21:55:04 +0300

    RegionDTO update

---
+++ NZWalks.API/Models/DTO/RegionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    public class RegionDto
+    {
+        public Guid Id { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+        public string? regionImageUrl { get; set; }
+    }
+}
