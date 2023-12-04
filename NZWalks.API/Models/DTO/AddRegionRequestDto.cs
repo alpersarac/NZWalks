8471a11af587f8e5a1dc7db84fedf3ec62512603
SARAC <alper.sarac42@gmail.com>
2023-12-04 22:43:15 +0300

    Max Min Length

--- NZWalks.API/Models/DTO/AddRegionRequestDto.cs
+++ NZWalks.API/Models/DTO/AddRegionRequestDto.cs
@@ -5,8 +5,8 @@ namespace NZWalks.API.Models.DTO
     public class AddRegionRequestDto
     {
         [Required]
-        [MaxLength(3)]
-        [MinLength(3,ErrorMessage = "Code lenght has to be 3 characters.")]
+        [MaxLength(3, ErrorMessage = "Code lenght has to be 3 characters.")]
+        [MinLength(3, ErrorMessage = "Code lenght has to be 3 characters.")]
         public string Code { get; set; }
         public string Name { get; set; }
         public string? RegionImageUrl { get; set; }
