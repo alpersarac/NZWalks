a09f7f62ff371eaa3428b339f2858e8bd4c66600
SARAC <alper.sarac42@gmail.com>
2023-12-04 22:45:00 +0300

    Validation for UpdateRegionRequestDto

--- NZWalks.API/Models/DTO/UpdateRegionRequestDto.cs
+++ NZWalks.API/Models/DTO/UpdateRegionRequestDto.cs
@@ -8,6 +8,8 @@ namespace NZWalks.API.Models.DTO
         [MaxLength(3, ErrorMessage = "Code lenght has to be 3 characters.")]
         [MinLength(3, ErrorMessage = "Code lenght has to be 3 characters.")]
         public string Code { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "Name lenght has to be 3 characters.")
         public string Name { get; set; }
         public string? RegionImageUrl { get; set; }
     }
