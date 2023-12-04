41e125f9e9489e302d0880ce63163de5cb7ade81
SARAC <alper.sarac42@gmail.com>
2023-12-04 22:44:36 +0300

    Validations for UpdateRegionRequestDto

--- NZWalks.API/Models/DTO/UpdateRegionRequestDto.cs
+++ NZWalks.API/Models/DTO/UpdateRegionRequestDto.cs
@@ -1,7 +1,12 @@
-﻿namespace NZWalks.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
 {
     public class UpdateRegionRequestDto
     {
+        [Required]
+        [MaxLength(3, ErrorMessage = "Code lenght has to be 3 characters.")]
+        [MinLength(3, ErrorMessage = "Code lenght has to be 3 characters.")]
         public string Code { get; set; }
         public string Name { get; set; }
         public string? RegionImageUrl { get; set; }
