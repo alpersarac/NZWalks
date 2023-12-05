e79fab44a41a0ba8521e458ad360a9ada43e7d3d
SARAC <alper.sarac42@gmail.com>
2023-12-05 19:10:05 +0300

    UpdateWalkRequestDto validation

---
+++ NZWalks.API/Models/DTO/UpdateWalkRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
+{
+    public class UpdateWalkRequestDto
+    {
+        [Required]    
+        public string Name { get; set; }
+        [Required] 
+        public string Description { get; set; }
+        [Required]
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+        [Required]
+        public Guid DifficultyId { get; set; }
+        [Required]
+        public Guid RegionId { get; set; }
+    }
+}
