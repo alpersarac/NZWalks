e845c825f913dea979979bd4794c99deacf777b1
SARAC <alper.sarac42@gmail.com>
2023-12-05 19:08:25 +0300

    AddWalkRequestDto validation limits

--- NZWalks.API/Models/DTO/AddWalkRequestDto.cs
+++ NZWalks.API/Models/DTO/AddWalkRequestDto.cs
@@ -5,10 +5,13 @@ namespace NZWalks.API.Models.DTO
     public class AddWalkRequestDto
     {
         [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
         [Required]
+        [MaxLength(100)]
         public string Description { get; set; }
         [Required]
+        [Range(0, 50)]
         public double LengthInKm { get; set; }
         public string? WalkImageUrl { get; set; }
         [Required]
