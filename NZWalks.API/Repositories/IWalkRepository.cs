64020abe10a92b3a0181fcbb4d86369f195d07c5
SARAC <alper.sarac42@gmail.com>
2023-12-09 17:00:30 +0300

    walkrepository filter implementation

--- NZWalks.API/Repositories/IWalkRepository.cs
+++ NZWalks.API/Repositories/IWalkRepository.cs
@@ -5,7 +5,7 @@ namespace NZWalks.API.Repositories
     public interface IWalkRepository
     {
         Task<Walk> CreateAsync(Walk walk);
-        Task<List<Walk>> GetAllAsync();
+        Task<List<Walk>> GetAllAsync(string? filterOn, string? filterQuery);
         Task<Walk> GetByIdAsync(Guid id);
         Task<Walk> UpdateAsync(Guid id, Walk walk);
         Task<Walk> DeleteAsync(Guid id);
