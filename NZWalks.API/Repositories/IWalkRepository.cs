cacfefd28a9124d945798a28459621f9942c3a5a
SARAC <alper.sarac42@gmail.com>
2023-12-09 22:49:01 +0300

    walk ascending not nullable

--- NZWalks.API/Repositories/IWalkRepository.cs
+++ NZWalks.API/Repositories/IWalkRepository.cs
@@ -5,7 +5,7 @@ namespace NZWalks.API.Repositories
     public interface IWalkRepository
     {
         Task<Walk> CreateAsync(Walk walk);
-        Task<List<Walk>> GetAllAsync(string? filterOn=null, string? filterQuery = null, string? sortBy=null, bool? isAscending=null);
+        Task<List<Walk>> GetAllAsync(string? filterOn=null, string? filterQuery = null, string? sortBy=null, bool isAscending=true);
         Task<Walk> GetByIdAsync(Guid id);
         Task<Walk> UpdateAsync(Guid id, Walk walk);
         Task<Walk> DeleteAsync(Guid id);
