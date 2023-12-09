b9519251444502d74d60ddac4dfff59bf17d4902
SARAC <alper.sarac42@gmail.com>
2023-12-09 17:09:49 +0300

    null by default for filtering

--- NZWalks.API/Repositories/IWalkRepository.cs
+++ NZWalks.API/Repositories/IWalkRepository.cs
@@ -5,7 +5,7 @@ namespace NZWalks.API.Repositories
     public interface IWalkRepository
     {
         Task<Walk> CreateAsync(Walk walk);
-        Task<List<Walk>> GetAllAsync(string? filterOn, string? filterQuery);
+        Task<List<Walk>> GetAllAsync(string? filterOn=null, string? filterQuery = null);
         Task<Walk> GetByIdAsync(Guid id);
         Task<Walk> UpdateAsync(Guid id, Walk walk);
         Task<Walk> DeleteAsync(Guid id);
