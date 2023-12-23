bfb75cd9f045afd46d91815611d0a294687a48cd
SARAC <alper.sarac42@gmail.com>
2023-12-23 17:05:50 +0300

    seeding roles

--- NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalks.API.Data
@@ -7,6 +8,32 @@ namespace NZWalks.API.Data
     {
         public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
         {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var readerRoleId = "8d251ebc-f3b8-4af7-9472-3a6c6fa0f9b0";
+            var writeRoleId = "b9e257ff-8051-46db-a079-bf2c19e467c8";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp=readerRoleId,
+                    Name="Reader",
+                    NormalizedName="Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id=writeRoleId, 
+                    ConcurrencyStamp=writeRoleId,
+                    Name="Writer",
+                    NormalizedName="Writer".ToUpper()
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
