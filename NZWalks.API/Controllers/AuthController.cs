78ad120bc5320f8dfd8ebe8bb171cc4cb200dbcf
SARAC <alper.sarac42@gmail.com>
2023-12-23 20:51:43 +0300

    AuthController

---
+++ NZWalks.API/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+                this._userManager = userManager;
+        }
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDTO)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDTO.username,
+                Email = registerRequestDTO.username
+            };
+            var identityUseR=await _userManager.CreateAsync(identityUser,registerRequestDTO.password);
+        }
+    }
+}
---
+++ NZWalks.API/Models/DTO/RegisterRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
+{
+    public class RegisterRequestDTO
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+}
