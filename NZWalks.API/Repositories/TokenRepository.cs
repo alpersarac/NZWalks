c1b9c3fe3564332cdd2eeeb6dc0f0e6d4b3366c8
SARAC <alper.sarac42@gmail.com>
2023-12-27 22:58:15 +0300

    token repository

---
+++ NZWalks.API/Repositories/TokenRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NZWalks.API.Repositories
+{
+    public class TokenRepository : ITokenRepository
+    {
+        private readonly IConfiguration _configuration;
+        public TokenRepository(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+        public string CreateJWTToken(IdentityUser user, List<string> roles)
+        {
+            //Create Claims
+
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Email, user.Email));
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role,role));
+            }
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials:credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
