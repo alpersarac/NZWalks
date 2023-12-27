120f1892f3d7b29839de47d0ce7aed58d38878d8
SARAC <alper.sarac42@gmail.com>
2023-12-27 21:35:02 +0300

    login dto

---
+++ NZWalks.API/Models/DTO/LoginRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
+{
+    public class LoginRequestDTO
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+}
